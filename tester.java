fe34055c6a58349b0eeaa20a5041b9a39d3fb4f4
cohyunoh <coh00@stuy.edu>
2019-03-11 19:14:59 -0400

    it doesnt really work

--- tester.java
+++ tester.java
@@ -1,13 +1,33 @@
+import java.util.Random;
 public class tester{
+  private static String toString(int[] ary){
+    String ans = "";
+    for(int i = 0; i < ary.length; i++){
+      ans += ary[i] + " ";
+    }
+    return ans;
+  }
+  private static boolean test(int[] ary){
+    for(int i = 0; i < ary.length - 1; i++){
+      if(ary[i] > ary[i + 1]){
+        return false;
+      }
+    }
+    return true;
+  }
   public static void main(String[] args) {
-    int[]ary = { 2, 10, 15, 23, 0,  5};  //sorted :  {0,2,5,10,15,23}
-    ary = Quick.quicksort(ary);
-    System.out.println(ary[0]);
-    System.out.println(ary[1]);
-    System.out.println(ary[2]);
-    System.out.println(ary[3]);
-    System.out.println(ary[4]);
-    System.out.println(ary[5]);
+    if(args[0] != null){
+      int len = Integer.parseInt(args[0]);
+      int[] ary = new int[len];
+      Random nums = new Random();
+      for(int i = 0; i < len; i++){
+        ary[i] = nums.nextInt() / 100000000;
+      }
+      System.out.println(toString(ary));
+      System.out.println(test(Quick.quicksort(ary)));
+      System.out.println(toString(ary));
+    }
+
     /*
     System.out.println(Quick.quickselect(ary , 0 ));
     System.out.println(Quick.quickselect(ary , 1 ));

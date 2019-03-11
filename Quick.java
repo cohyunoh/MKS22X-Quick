6df84c9621134d447559f1fc7457faed928d7b42
cohyunoh <coh00@stuy.edu>
2019-03-11 11:44:50 -0400

    added my partition method I need for quickselect

--- Quick.java
+++ Quick.java
@@ -2,6 +2,47 @@ public class Quick{
   /*return the value that is the kth smallest value of the array.
  */
  public static int quickselect(int []data, int k){
-   
+
+ }
+ public static int partition ( int [] data, int start, int end){
+   if(end == start){
+     return start;
+   }
+   int pivotIndex = ((int)(Math.random() * 10) % (end + 1 - start)) + start;
+   int first =  data[start];
+   data[start] = data[pivotIndex];
+   data[pivotIndex] = first;
+   pivotIndex = start;
+   int tempStart = start + 1;
+   int tempEnd = end;
+   //System.out.println(pivotIndex);
+   while(tempStart != tempEnd){
+     //System.out.println("Start: " + tempStart);
+     //System.out.println("End: " + tempEnd);
+     //System.out.println(toString(data));
+     if(data[tempStart] > data[pivotIndex]){
+       int temp = data[tempStart];
+       data[tempStart] = data[tempEnd];
+       data[tempEnd] = temp;
+       tempEnd --;
+     }else if(data[tempStart] <= data[pivotIndex]){
+       tempStart ++;
+     }
+     //System.out.println("Start: " + tempStart);
+     //System.out.println("End: " + tempEnd);
+     //System.out.println(toString(data));
+   }
+   for(int c = start; c < end + 1; c++){
+     if(data[c] > data[pivotIndex]){
+       int temp = data[c - 1];
+       data[c - 1] = data[pivotIndex];
+       data[pivotIndex] = temp;
+       return c - 1;
+     }
+   }
+   int temp = data[end];
+   data[end] = data[pivotIndex];
+   data[pivotIndex] = temp;
+   return end;
  }
 }

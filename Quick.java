1df7018771c233b9474f6658a0426084c1b0420b
cohyunoh <coh00@stuy.edu>
2019-03-13 11:23:50 -0400

    realized that quicksort is a void method

--- Quick.java
+++ Quick.java
@@ -36,9 +36,9 @@ public class Quick{
    return data[output];
  }
 
- public static int[] quicksort(int[] data){
+ public static void quicksort(int[] data){
    //perform recursive helper method with the start and end being the whole list
-   return quickH(data, 0, data.length - 1);
+   quickH(data, 0, data.length - 1);
  }
 
 /*
@@ -51,29 +51,23 @@ public class Quick{
  }
  */
 
- private static int[] quickH(int[] data, int start, int end){
+ private static void quickH(int[] data, int start, int end){
    //if the two start and end are equal then that means that the element that is in this
    //index is in the right place
-   if(end == start){
-     return data;
+   if(end <= start){
+     return ;
    }else{
      //System.out.println("Pass");
      //System.out.println(toString(data));
-    // System.out.println(start);
+     // System.out.println(start);
      //System.out.println(end);
      //get the newpivot by retrieving the value from a partitioned list from start to end
      int newpivot = partition(data, start, end);
      //perform another partition with the less half of the values
-     if(newpivot - 1 > start){
-       quickH(data, start, newpivot - 1);
-     }
+     quickH(data, start, newpivot - 1);
      //perform another partition with great hald of the values
-     if(newpivot + 1 < end){
-       quickH(data, newpivot + 1, end);
-     }
+     quickH(data, newpivot + 1, end);
    }
-
-   return data;
  }
 
  public static int partition ( int [] data, int start, int end){
